5f74eeca5754745d1ea395149e300a8e49ff8c96
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-12-25 18:05:22 +0300

    DB'deki sesleri filtreleme ve oynatma eklendi

--- VoiceCommandDatabase/mainpage/urls.py
+++ VoiceCommandDatabase/mainpage/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # http://127.0.0.1:8000 => mainpage
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path("ses_ekle/", views.add_voice, name="add_voice"),
     path("ses_kaydet/", views.record_voice, name="record_voice"),
     path("sesleri_goruntule/", views.show_voices, name="show_voices"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
